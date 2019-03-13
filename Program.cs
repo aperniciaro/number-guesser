c7b4fea3ecf7c0557a2b12796369abd33e26ad8e
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-13 14:16:31 -0400

    code working, adding to github

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace number_guesser
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var min = 0;
+      var max = 100;
+      var lower = min;
+      var upper = max;
+      var guess = (upper - lower) / 2;
+      var response = "";
+
+      Console.WriteLine($"Think of a number between {min} and {max}");
+      while (response != "equal")
+      {
+        Console.WriteLine($"Is your number higher, lower, or equal to {guess}?");
+        response = Console.ReadLine();
+        if (response == "higher")
+        {
+          lower = guess;
+        }
+        else if (response == "lower")
+        {
+          upper = guess;
+        }
+        guess = (upper + lower) / 2;
+      }
+      Console.WriteLine($"Your number is {guess}!");
+    }
+  }
+}
